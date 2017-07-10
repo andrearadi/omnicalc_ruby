c24b58190768c99ac8403a1428d845b402bd2be0
AndreaRadi <aradi@chicagobooth.edu>
2017-07-10 14:32:21 +0000

    median

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -98,8 +98,15 @@ class CalculationsController < ApplicationController
     @maximum = @numbers.max
 
     @range = (@numbers.max)- (@numbers.min)
+    
+    def median(array)
+    sorted = array.sort
+    len = sorted.length
+    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+    end
 
-    @median = 
+    @median = median(@numbers)
+
 
     @sum = @numbers.sum
 
